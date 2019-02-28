8ccf9a709779cfc5a6cefa6466fd44ecb696033d
RicardoCastilloISC <ricardocastilloisc@gmail.com>
2019-02-28 18:38:54 -0500

    15. ReactJS: de cero a experto. state

--- src/App.js
+++ src/App.js
@@ -4,16 +4,28 @@ import './App.css';
 import Cabecera from './components/Cabecera';
 
 class App extends Component {
-  manejaClick = (texto) => {
+  state = {
+    miau: 'Bienvenido a miau',
+  };
+
+  cambiarTextoDelEstado = () => {
+    this.setState({miau: 'hola mundo'});
+  };
+  manejaClick = texto => {
     console.log(texto);
   };
   render() {
-    const miau = 'Bienvenido miau';
+    const {miau} = this.state;
+
+    const texto = 'Bienvenido texto';
+    const guau = 'Bienvenido gua';
 
     return (
       <div className="App">
         <Cabecera miau={miau} manejaClick={this.manejaClick} />
-        <p className="App-intro">Hola mundo</p>
+        <p onClick={this.cambiarTextoDelEstado} className="App-intro">
+          {miau}
+        </p>
       </div>
     );
   }
