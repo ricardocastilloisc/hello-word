ea4a8ef984ef670b2b5b23aaf495bf559d476485
RicardoCastilloISC <ricardocastilloisc@gmail.com>
2019-03-04 12:43:25 -0500

    19.ReactJS: de cero a experto. Inline style dinámico

--- src/components/Cabecera.jsx
+++ src/components/Cabecera.jsx
@@ -2,8 +2,8 @@ import React, {Component} from 'react';
 import logo from '../logo.svg';
 
 const styles = {
-  header: {
-    backgroundColor: '#282c34',
+  header: ({backgroundColor}) => ({
+    backgroundColor,
     minHeight: '100vh',
     display: 'flex',
     flexDirection: 'column',
@@ -11,10 +11,18 @@ const styles = {
     justifyContent: 'center',
     fontSize: 'calc(10px + 2vmin)',
     color: 'white',
-  },
+  })
 };
 
 export default class Cabecera extends Component {
+  state =
+  {
+    backgroundColor: '#222'
+  }
+  cambiarColorHeader = () =>
+  {
+    this.setState({backgroundColor: '#f00'})
+  }
   manejaClick = () => {
     const {miau, manejaClick} = this.props;
     manejaClick (miau);
@@ -22,8 +30,9 @@ export default class Cabecera extends Component {
 
   render () {
     const {miau} = this.props;
+    const {backgroundColor} = this.state;
     return (
-      <header style={styles.header}>
+      <header onClick={this.cambiarColorHeader} style={styles.header({backgroundColor})}>
         <img
           onClick={this.manejaClick}
           src={logo}
