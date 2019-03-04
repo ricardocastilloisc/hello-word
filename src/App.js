8e696d0a2c9e4a7d7fafff59d94b5b3f15b487c3
RicardoCastilloISC <ricardocastilloisc@gmail.com>
2019-03-04 16:01:52 -0500

    24.-ReactJS: de cero a experto. API Fetch

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import React, {Component} from 'react';
 import FormularioSimple from './components/FormularioSimple';
-
-
+import {stringify} from 'postcss';
 
 class App extends Component {
+  constructor () {
+    super ();
+    //console.log ('aca haremos llamados ajax con fetch');
 
-  render() {
+    //get/post put
+    /*    fetch ('https://jsonplaceholder.typicode.com/users')
+      .then (x => x.json ())
+      .then (x => console.log (x)); */
+
+    fetch ('https://jsonplaceholder.typicode.com/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify ({
+        name: 'Ricardo',
+        username: 'richar',
+      }),
+    })
+      .then (x => x.json ())
+      .then (x => console.log (x));
+  }
+
+  render () {
     return (
       <div className="App">
-      <FormularioSimple />
-      
+        <FormularioSimple />
       </div>
     );
   }
