f54816c73ea077e63de3069dd4c832991fc9a622
RicardoCastilloISC <ricardocastilloisc@gmail.com>
2019-03-04 16:10:46 -0500

    25.-ReactJS: de cero a experto. Axios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 import FormularioSimple from './components/FormularioSimple';
 import {stringify} from 'postcss';
 
 class App extends Component {
   constructor () {
     super ();
+
+    axios.post('https://jsonplaceholder.typicode.com/users',{
+      name: 'Ricardo',
+      username: 'richar',
+    })
+    .then(({data}) => {
+      console.log(data)
+    })
+    .catch(err => {
+      console.error(err); 
+    })
+
+/*     axios.get('https://jsonplaceholder.typicode.com/users')
+    .then(({data}) => {
+      console.log(data)
+    })
+    .catch(err => {
+      console.error(err); 
+    }) */
     //console.log ('aca haremos llamados ajax con fetch');
 
     //get/post put
@@ -12,7 +32,7 @@ class App extends Component {
       .then (x => x.json ())
       .then (x => console.log (x)); */
 
-    fetch ('https://jsonplaceholder.typicode.com/users', {
+/*     fetch ('https://jsonplaceholder.typicode.com/users', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -24,8 +44,8 @@ class App extends Component {
     })
       .then (x => x.json ())
       .then (x => console.log (x));
+  } */
   }
-
   render () {
     return (
       <div className="App">
